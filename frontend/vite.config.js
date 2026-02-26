e24a486fbc7b431dc6bffb6574d2c7dbb54459fb
vimsd <cheertahh@gmail.com>
2026-02-26 23:53:38 +0700

    fix docker and add show&seat to database

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,          // ⭐ สำคัญมากสำหรับ Docker
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://cinema_backend:5000', // ⭐ ชื่อ service backend
         changeOrigin: true,
         secure: false,
       },
--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS payments;
 DROP TABLE IF EXISTS book_seat;
-DROP TABLE IF EXISTS seats;
 DROP TABLE IF EXISTS booking;
+DROP TABLE IF EXISTS seats;
 DROP TABLE IF EXISTS showtimes;
 DROP TABLE IF EXISTS users;
 
@@ -13,7 +13,6 @@ CREATE TABLE users (
 );
 
 -- SHOWTIMES
--- movie_id, theater_id refer to NoSQL
 CREATE TABLE showtimes (
     showtime_id INT AUTO_INCREMENT PRIMARY KEY,
     movie_id INT NOT NULL,
@@ -27,7 +26,6 @@ CREATE TABLE booking (
     user_id INT NOT NULL,
     showtime_id INT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (showtime_id) REFERENCES showtimes(showtime_id)
 );
@@ -45,11 +43,9 @@ CREATE TABLE book_seat (
     showtime_id INT NOT NULL,
     book_id INT NOT NULL,
     seat_id INT NOT NULL,
-
     FOREIGN KEY (showtime_id) REFERENCES showtimes(showtime_id),
     FOREIGN KEY (book_id) REFERENCES booking(book_id),
     FOREIGN KEY (seat_id) REFERENCES seats(seat_id),
-
     UNIQUE (showtime_id, seat_id)
 );
 
@@ -59,11 +55,20 @@ CREATE TABLE payments (
     book_id INT NOT NULL,
     amount DECIMAL(10,2) NOT NULL,
     status ENUM('Failed', 'Pending', 'Paid') DEFAULT 'Pending',
-
     FOREIGN KEY (book_id) REFERENCES booking(book_id)
 );
 
 
+INSERT INTO users (email, password)
+VALUES ('user1@gmail.com', 'user1');
 
-insert value into users(1001 , "user1@gmail.com" , "user1");
+INSERT INTO showtimes (movie_id, theater_id, showtime)
+VALUES
+  (1, 1, '2026-03-01 18:00:00'),
+  (1, 1, '2026-03-01 20:30:00'),
+  (2, 1, '2026-03-02 19:00:00');
 
+INSERT INTO seats (theater_id, seat)
+VALUES
+  (1, 'A1'), (1, 'A2'), (1, 'A3'), (1, 'A4'), (1, 'A5'),
+  (1, 'B1'), (1, 'B2'), (1, 'B3'), (1, 'B4'), (1, 'B5');
