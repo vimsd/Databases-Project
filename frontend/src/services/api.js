d9816981dfa548b76272a65241cb5e906d88908c
vimsd <cheertahh@gmail.com>
2026-02-28 23:58:37 +0700

    Cleanup: remove unused frontend/api.js

--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// base path will be proxied by vite server; allow override via env var
-const API_URL = import.meta.env.VITE_API || '/api';
-
-export const getMovies = () => axios.get(`${API_URL}/movies`);
-export const createMovie = (data) => axios.post(`${API_URL}/movies`, data); //
-export const deleteMovie = (id) => axios.delete(`${API_URL}/movies/${id}`);
