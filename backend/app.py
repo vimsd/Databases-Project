0f92669f6411b40e7234337a51b17402ed4f44a7
Punnawich <Kurosougun@gmail.com>
2026-04-03 22:09:57 +0700

    deploy

--- backend/app.py
+++ backend/app.py
@@ -40,4 +40,5 @@ def serve_frontend(path):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
